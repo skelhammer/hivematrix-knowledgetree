073aaf5d25d2c829e49eb740078ecc288aed3902
Skelhammer <kitsunenomen@gmail.com>
2025-11-20 19:59:48 -0800

    Update rate limits for local LAN deployment

--- app/__init__.py
+++ app/__init__.py
@@ -114,7 +114,7 @@ from flask_limiter.util import get_remote_address
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
-    default_limits=["200 per hour", "50 per minute"],
+    default_limits=["10000 per hour", "500 per minute"],
     storage_uri="memory://"
 )
 
